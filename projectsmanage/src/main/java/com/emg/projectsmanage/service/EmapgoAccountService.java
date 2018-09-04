c0f81a89cb7e6dcf42594cbbe02e0e935b06ca41
swimmingcoach <zshang_0630@sina.com>
2018-09-04 11:54:35 +0800

    人员权限相关不在使用缓存 脱裤子放屁的事

--- projectsmanage/src/main/java/com/emg/projectsmanage/service/EmapgoAccountService.java
+++ projectsmanage/src/main/java/com/emg/projectsmanage/service/EmapgoAccountService.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.emg.projectsmanage.dao.emapgoaccount.AuthorityModelDao;
@@ -16,8 +15,6 @@ import com.emg.projectsmanage.pojo.EmployeeModel;
 
 @Service
 public class EmapgoAccountService {
-	final static private String CACHEVALUE = "EmapAccountCache";
-	final static private String CACHEKEYGENERATOR = "baseCacheKeyGenerator";
 
 	@Autowired
 	private DepartmentModelDao departmentModelDao;
@@ -28,29 +25,24 @@ public class EmapgoAccountService {
 	@Autowired
 	private AuthorityModelDao authorityModelDao;
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public List<DepartmentModel> getAllDepartment() {
 		return departmentModelDao.getAllDepartment();
 	}
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public List<EmployeeModel> getAllEmployees() {
 		return employeeModelDao.getAllEmployees();
 	}
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public EmployeeModel getOneEmployee(EmployeeModel record) {
 		return employeeModelDao.getOneEmployee(record);
 	}
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public List<EmployeeModel> getEmployeeByIDS(List<Integer> ids) {
 		Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
 		map.put("ids", ids);
 		return employeeModelDao.getEmployeeByIDS(map);
 	}
 	
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public List<EmployeeModel> getEmployeesByIDSAndRealname(List<Integer> ids, String realname) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("ids", ids);
@@ -59,12 +51,10 @@ public class EmapgoAccountService {
 		return employeeModelDao.getEmployeesByIDSAndRealname(map);
 	}
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public List<Map<String, Object>> getEmployeeListForZTree() {
 		return employeeModelDao.getEmployeeListForZTree();
 	}
 	
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public AuthorityModel getAuthorityByUsername(String username) {
 		return authorityModelDao.getAuthorityByUsername(username);
 	}
--- projectsmanage/src/main/java/com/emg/projectsmanage/service/ProjectManagerRoleService.java
+++ projectsmanage/src/main/java/com/emg/projectsmanage/service/ProjectManagerRoleService.java
@@ -3,8 +3,6 @@ package com.emg.projectsmanage.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.emg.projectsmanage.common.RoleType;
@@ -13,13 +11,10 @@ import com.emg.projectsmanage.pojo.RoleModel;
 
 @Service
 public class ProjectManagerRoleService {
-	final static private String CACHEVALUE = "ProjectManagerRoleCache";
-	final static private String CACHEKEYGENERATOR = "baseCacheKeyGenerator";
 
 	@Autowired
 	private RoleModelDao roleModelDao;
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public RoleModel getWorkerRole() {
 		RoleModel roleparam = new RoleModel();
 		roleparam.setName(RoleType.ROLE_WORKER.toString());
@@ -31,7 +26,6 @@ public class ProjectManagerRoleService {
 			return null;
 	}
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public RoleModel getCheckerRole() {
 		RoleModel roleparam = new RoleModel();
 		roleparam.setName(RoleType.ROLE_CHECKER.toString());
@@ -43,14 +37,12 @@ public class ProjectManagerRoleService {
 			return null;
 	}
 
-	@Cacheable(value = CACHEVALUE, keyGenerator = CACHEKEYGENERATOR)
 	public List<RoleModel> getAllEnabledRoles() {
 		RoleModel roleparam = new RoleModel();
 		roleparam.setEnabled(1);
 		return roleModelDao.queryRoles(roleparam);
 	}
 
-	@CacheEvict(value = CACHEVALUE, allEntries = true)
 	public int addRole(RoleModel record) {
 		return roleModelDao.addRole(record);
 	}
