31ae405db5a492045ede3df4d6e33e5bc8cd2886
swimmingcoach <zshang_0630@sina.com>
2018-09-25 11:41:32 +0800

    现在关于3个默认用户的配置不再依赖spring-security了，自己实现了

--- projectsmanage/src/main/java/com/emg/projectsmanage/auth/CustomUserDetailsService.java
+++ projectsmanage/src/main/java/com/emg/projectsmanage/auth/CustomUserDetailsService.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Map;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
+import com.emg.projectsmanage.common.RoleType;
 import com.emg.projectsmanage.dao.projectsmanager.UserRoleModelDao;
 import com.emg.projectsmanage.pojo.AuthorityModel;
 import com.emg.projectsmanage.service.EmapgoAccountService;
@@ -19,6 +22,21 @@ import com.emg.projectsmanage.service.SessionService;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
+
+	private final static String SUPERADMIN_USERNAME = "superadmin";
+	@Value("${superadmin.password}")
+	private String SUPERADMIN_PASSWORD = "emapgo1qaz@WSX";
+	private final static RoleType SUPERADMIN_ROLETYPE = RoleType.ROLE_SUPERADMIN;
+
+	private final static String YANFAADMIN_USERNAME = "yanfaadmin";
+	@Value("${yanfaadmin.password}")
+	private String YANFAADMIN_PASSWORD = "5921034";
+	private final static RoleType YANFAADMIN_ROLETYPE = RoleType.ROLE_YANFAADMIN;
+
+	private final static String ADMIN_USERNAME = "admin";
+	@Value("${admin.password}")
+	private String ADMIN_PASSWORD = "emapgo123!@#";
+	private final static RoleType ADMIN_ROLETYPE = RoleType.ROLE_ADMIN;
 
 	@Autowired
 	private EmapgoAccountService emapgoAccountService;
@@ -31,12 +49,45 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Override
 	public CustomUserDetails loadUserByUsername(String username) {
+
+		if (sessionService.isDuplicateLogin(username)) {
+			throw new SessionAuthenticationException(new String());
+		}
+
+		if (username.equals(SUPERADMIN_USERNAME)) {
+			CustomUserDetails userDetails = new CustomUserDetails();
+			userDetails.setUsername(username);
+			userDetails.setPassword(DigestUtils.md5DigestAsHex(SUPERADMIN_PASSWORD.getBytes()));
+			userDetails.setEnabled(Boolean.valueOf(true));
+			List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
+			auths.add(new SimpleGrantedAuthority(SUPERADMIN_ROLETYPE.toString()));
+			userDetails.setAuthorities(auths);
+			return userDetails;
+		}
+		if (username.equals(YANFAADMIN_USERNAME)) {
+			CustomUserDetails userDetails = new CustomUserDetails();
+			userDetails.setUsername(username);
+			userDetails.setPassword(DigestUtils.md5DigestAsHex(YANFAADMIN_PASSWORD.getBytes()));
+			userDetails.setEnabled(Boolean.valueOf(true));
+			List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
+			auths.add(new SimpleGrantedAuthority(YANFAADMIN_ROLETYPE.toString()));
+			userDetails.setAuthorities(auths);
+			return userDetails;
+		}
+		if (username.equals(ADMIN_USERNAME)) {
+			CustomUserDetails userDetails = new CustomUserDetails();
+			userDetails.setUsername(username);
+			userDetails.setPassword(DigestUtils.md5DigestAsHex(ADMIN_PASSWORD.getBytes()));
+			userDetails.setEnabled(Boolean.valueOf(true));
+			List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
+			auths.add(new SimpleGrantedAuthority(ADMIN_ROLETYPE.toString()));
+			userDetails.setAuthorities(auths);
+			return userDetails;
+		}
+
 		AuthorityModel authority = emapgoAccountService.getAuthorityByUsername(username);
 		if (authority == null) {
 			throw new UsernameNotFoundException(new String());
-		}
-		if(sessionService.isDuplicateLogin(username)) {
-			throw new SessionAuthenticationException(new String());
 		}
 
 		CustomUserDetails userDetails = new CustomUserDetails();
