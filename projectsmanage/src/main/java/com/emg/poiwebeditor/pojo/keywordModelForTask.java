56bdebeb3232396f0584c84f5f0e2e2a1521eca5
Administrator <243210740@qq.com>
2019-05-16 15:50:29 +0800

    144141

---
+++ projectsmanage/src/main/java/com/emg/poiwebeditor/pojo/keywordModelForTask.java
@@ -0,0 +1,12 @@
+package com.emg.poiwebeditor.pojo;
+
+public class keywordModelForTask {
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
